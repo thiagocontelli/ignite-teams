0a3ab57869c088a9de48111641fb38b814d26642
Thiago Contelli <thiagocontellid@gmail.com>
2022-09-15 18:27:15 -0300

    CSS-in-JS > Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
-import theme from '@theme/index'
+import theme from '@theme/index';
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 	return (
 		<ThemeProvider theme={theme}>
-			<Groups />
+			{fontsLoaded ? <Groups /> : <ActivityIndicator />}
 		</ThemeProvider>
 	);
 }
