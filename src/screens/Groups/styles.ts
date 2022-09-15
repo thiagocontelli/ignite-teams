d6b4e6ae06b52f475d214b16dc93042fe2c73f63
Thiago Contelli <thiagocontellid@gmail.com>
2022-09-15 17:48:07 -0300

    CSS-in-JS > Alias de Importação

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1;
+	font-size: large;
+	align-items: center;
+	justify-content: center;
+	background-color: red;
+`;
+
+export const Title = styled.Text`
+	color: #ffff;
+`;
