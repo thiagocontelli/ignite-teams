da5bce9921f28436b3a53273195dfbb7ebe9d998
Thiago <thiagocontellid@gmail.com>
2022-09-16 14:19:36 -0300

    Navegação > Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,15 +3,19 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
-import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
 	const [groups, setGroups] = useState([]);
 
 	const navigation = useNavigation();
+
+	function handleNewGroup() {
+		navigation.navigate('new');
+	}
 
 	return (
 		<Container>
@@ -27,7 +31,7 @@ export function Groups() {
 				contentContainerStyle={groups.length === 0 && { flex: 1 }}
 				showsVerticalScrollIndicator={false}
 			/>
-			<Button title="Criar nova turma" />
+			<Button title="Criar nova turma" onPress={handleNewGroup} />
 		</Container>
 	);
 }
