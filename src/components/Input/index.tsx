06fae78824a425eb8d3dc60eca9619e75a58f71b
Thiago <thiagocontellid@gmail.com>
2022-09-16 09:48:56 -0300

    CSS-in-JS > Criando componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./style";
+
+
+export function Input({...rest}: TextInputProps) {
+  return (
+    <Container {...rest} />
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
 import { Container, Content, Icon } from './style';
 
 export function NewGroup() {
@@ -14,7 +15,8 @@ export function NewGroup() {
 					title="Nova Turma"
 					subtitle="Crie a turma para adicionar as pessoas"
 				/>
-				<Button title="Criar" />
+        <Input />
+				<Button title="Criar" style={{marginTop: 20}} />
 			</Content>
 		</Container>
 	);
