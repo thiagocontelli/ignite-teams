0bacdd31e1ad17b2220f03ef01440fd9b234d5a4
Thiago <thiagocontellid@gmail.com>
2022-09-20 14:33:18 -0300

    Local Storage > Filtrando por time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
@@ -46,8 +46,8 @@ export function Players() {
 
 		try {
 			await playerAddByGroup(newPlayer, group);
-			const players = await playersGetByGroup(group);
-			console.log(players);
+			// const players = await playersGetByGroup(group);
+			fetchPlayersByTeam();
 		} catch (error) {
 			if (error instanceof AppError) {
 				Alert.alert('Nova pessoa', error.message);
@@ -74,6 +74,10 @@ export function Players() {
 			);
 		}
 	}
+
+	useEffect(() => {
+		fetchPlayersByTeam();
+	}, [team]);
 
 	return (
 		<Container>
@@ -110,9 +114,9 @@ export function Players() {
 
 			<FlatList
 				data={players}
-				keyExtractor={(item) => item}
+				keyExtractor={(item) => item.name}
 				renderItem={({ item }) => (
-					<PlayerCard name={item} onRemove={() => {}} />
+					<PlayerCard name={item.name} onRemove={() => {}} />
 				)}
 				ListEmptyComponent={() => (
 					<ListEmpty message="Não há pessoas nesse time" />
